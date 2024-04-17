d170163e701a99803cde530ced563520dbc1c4c2
vahid torkaman <vahidt@spotify.com>
2024-04-17 16:51:13 +0200

    add the same types constructor for the list type in confidence value

--- Provider/src/main/java/com/spotify/confidence/ConfidenceValue.kt
+++ Provider/src/main/java/com/spotify/confidence/ConfidenceValue.kt
@@ -23,7 +23,7 @@ sealed interface ConfidenceValue {
     data class Struct(val map: Map<kotlin.String, ConfidenceValue>) : ConfidenceValue
 
     @Serializable
-    data class List(val list: kotlin.collections.List<ConfidenceValue>) : ConfidenceValue
+    data class List internal constructor(val list: kotlin.collections.List<ConfidenceValue>) : ConfidenceValue
 
     @Serializable
     data class Date(@Serializable(DateSerializer::class) val date: java.util.Date) : ConfidenceValue
@@ -33,4 +33,20 @@ sealed interface ConfidenceValue {
 
     @Serializable
     object Null : ConfidenceValue
+
+    companion object {
+        fun stringList(list: kotlin.collections.List<kotlin.String>) =
+            List(list.map(::String))
+        fun doubleList(list: kotlin.collections.List<kotlin.Double>) =
+            List(list.map(::Double))
+        fun booleanList(list: kotlin.collections.List<kotlin.Boolean>) =
+            List(list.map(::Boolean))
+        fun integerList(list: kotlin.collections.List<kotlin.Int>) =
+            List(list.map(::Integer))
+
+        fun dateList(list: kotlin.collections.List<java.util.Date>) =
+            List(list.map(::Date))
+        fun timestampList(list: kotlin.collections.List<java.util.Date>) =
+            List(list.map(::Timestamp))
+    }
 }
