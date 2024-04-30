210fbdd3230170e3768b835a7c4e69ac103046a6
vahid torkaman <vahidt@spotify.com>
2024-04-30 10:20:07 +0200

    add maven release block for confidence module

--- confidence/build.gradle.kts
+++ confidence/build.gradle.kts
@@ -1,26 +1,12 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("signing")
     kotlin("plugin.serialization").version("1.8.10").apply(true)
 }
 
 val providerVersion = project.extra["version"].toString()
-
-sourceSets {
-    create("testUtil") {
-        java.srcDir("src/testUtil/kotlin")
-        resources.srcDir("src/testUtil/resources")
-    }
-}
-
-tasks.register<Jar>("testUtilJar") {
-    archiveClassifier.set("test-util")
-    from(sourceSets["testUtil"].output)
-}
-
-artifacts {
-    add("archives", tasks["testUtilJar"])
-}
 
 object Versions {
     const val openFeatureSDK = "0.2.3"
@@ -44,21 +30,19 @@ android {
         buildConfigField("String", "SDK_VERSION", "\"" + providerVersion + "\"")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
     }
 }
 
@@ -72,4 +56,64 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}")
     testImplementation("org.mockito.kotlin:mockito-kotlin:${Versions.kotlinMockito}")
     testImplementation("com.squareup.okhttp3:mockwebserver:${Versions.mockWebServer}")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = project.extra["groupId"].toString()
+            artifactId = "spotify-confidence-kotlin"
+            version = providerVersion
+
+            pom {
+                name.set("Confidence SDK Android")
+                description.set("Android SDK for Confidence")
+                url.set("https://github.com/spotify/confidence-openfeature-provider-kotlin")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("vahidlazio")
+                        name.set("Vahid Torkaman")
+                        email.set("vahidt@spotify.com")
+                    }
+                    developer {
+                        id.set("fabriziodemaria")
+                        name.set("Fabrizio Demaria")
+                        email.set("fdema@spotify.com")
+                    }
+                    developer {
+                        id.set("nicklasl")
+                        name.set("Nicklas Lundin")
+                        email.set("nicklasl@spotify.com")
+                    }
+                    developer {
+                        id.set("nickybondarenko")
+                        name.set("Nicky Bondarenko")
+                        email.set("nickyb@spotify.com")
+                    }
+                }
+                scm {
+                    connection.set(
+                        "scm:git:git://spotify/confidence-openfeature-provider-kotlin.git"
+                    )
+                    developerConnection.set(
+                        "scm:git:ssh://spotify/confidence-openfeature-provider-kotlin.git"
+                    )
+                    url.set("https://github.com/spotify/confidence-openfeature-provider-kotlin")
+                }
+            }
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["release"])
 }
