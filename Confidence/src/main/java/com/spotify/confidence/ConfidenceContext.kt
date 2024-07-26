054fb0fd397eed410b0ae30ebb412c7c897df4ac
Nicky <31034418+nickybondarenko@users.noreply.github.com>
2024-07-26 15:34:03 +0200

    docs: Add docs to public APIs (#179)

--- Confidence/src/main/java/com/spotify/confidence/ConfidenceContext.kt
+++ Confidence/src/main/java/com/spotify/confidence/ConfidenceContext.kt
@@ -1,15 +1,37 @@
 package com.spotify.confidence
 
 interface ConfidenceContextProvider {
+    /**
+     * @return context of given instance, including parent context if applicable.
+     */
     fun getContext(): Map<String, ConfidenceValue>
 }
 
 typealias ConfidenceFieldsType = Map<String, ConfidenceValue>
 
 interface Contextual : ConfidenceContextProvider {
+    /**
+     * Create a new Confidence child instance based on an existing Confidence instance
+     * @param context additional context.
+     */
     fun withContext(context: Map<String, ConfidenceValue>): Contextual
 
+    /**
+     * Add entry to context
+     * @param context context to add.
+     */
     fun putContext(context: Map<String, ConfidenceValue>)
+
+    /**
+     * Add entry to context
+     * @param key key of the entry.
+     * @param value value of the entry.
+     */
     fun putContext(key: String, value: ConfidenceValue)
+
+    /**
+     * Remove entry from context
+     * @param key key of the context to be removed.
+     */
     fun removeContext(key: String)
 }
