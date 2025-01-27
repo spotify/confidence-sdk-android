97c763c6bd2f07249a808d44cdbba63cba58fc2e
Nicklas Lundin <nicklasl@spotify.com>
2025-01-27 11:38:17 +0100

    feat(Provider): Add support for Tracking from OpenFeature (#190)
    * feat(Provider): add support for tracking in the Openfeature provider
    
    * fix: replace print with logger

--- Provider/src/test/java/com/spotify/confidence/openfeature/ProviderIntegrationTest.kt
+++ Provider/src/test/java/com/spotify/confidence/openfeature/ProviderIntegrationTest.kt
@@ -3,18 +3,23 @@ package com.spotify.confidence.openfeature
 import android.content.Context
 import com.spotify.confidence.ConfidenceFactory
 import dev.openfeature.sdk.ImmutableContext
+import dev.openfeature.sdk.ImmutableStructure
 import dev.openfeature.sdk.OpenFeatureAPI
 import dev.openfeature.sdk.Reason
+import dev.openfeature.sdk.TrackingEventDetails
 import dev.openfeature.sdk.Value
 import dev.openfeature.sdk.events.EventHandler
 import dev.openfeature.sdk.events.OpenFeatureEvents
 import dev.openfeature.sdk.exceptions.ErrorCode
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -176,5 +181,61 @@ class ProviderIntegrationTest {
 
         assertEquals(4, evaluationDetails.value.asStructure()?.getOrDefault("my-integer", Value.Integer(-1))?.asInteger())
     }
+
+    @Test
+    fun testEventTracking() = runTest {
+        val testDispatcher = StandardTestDispatcher(testScheduler)
+        val eventsHandler = EventHandler(Dispatchers.IO).apply {
+            publish(OpenFeatureEvents.ProviderStale)
+        }
+        val cacheDir = mockContext.getDir("events", Context.MODE_PRIVATE)
+        assertTrue(cacheDir.isDirectory)
+        assertTrue(cacheDir.listFiles().isEmpty())
+        val mockConfidence = ConfidenceFactory.create(mockContext, clientSecret, dispatcher = testDispatcher)
+
+        OpenFeatureAPI.setProvider(
+            ConfidenceFeatureProvider.create(
+                confidence = mockConfidence,
+                initialisationStrategy = InitialisationStrategy.ActivateAndFetchAsync,
+                eventHandler = eventsHandler
+            ),
+            ImmutableContext(
+                targetingKey = UUID.randomUUID().toString(),
+                attributes = mutableMapOf(
+                    "user" to Value.Structure(
+                        mapOf(
+                            "country" to Value.String("SE")
+                        )
+                    )
+                )
+            )
+        )
+        runBlocking {
+            awaitProviderReady(eventsHandler = eventsHandler)
+        }
+
+        assertEquals(1, cacheDir.listFiles()?.size)
+        assertEquals(0, cacheDir.listFiles()?.first()?.readLines()?.size)
+
+        OpenFeatureAPI.getClient().track("MyEventName", TrackingEventDetails(33.0, ImmutableStructure("key" to Value.String("value"))))
+        testScheduler.advanceUntilIdle()
+        val lines = cacheDir.listFiles()?.first()?.readLines() ?: emptyList()
+        assertEquals(1, lines.size)
+        val jsonString = lines.first()
+        assertTrue(jsonString.contains("\"eventDefinition\":\"MyEventName\""))
+        println(lines.first())
+        assertTrue(jsonString.contains("\"payload\":{\"value\":{\"double\":33.0},\"key\":{\"string\":\"value\"}"))
+        val regex = Regex(
+            "\"context\":\\{\"map\":\\{\"visitor_id\":\\{\"string\":\"[a-f0-9\\-]+\"}," +
+                "\"targeting_key\":\\{\"string\":\"[a-f0-9\\-]+\"}," +
+                "\"user\":\\{\"map\":\\{\"country\":\\{\"string\":\"SE\"}}}}}"
+        )
+        assertTrue(
+            "Expected the context map to match the regex for visitor_id and targeting_key. Actual JSON:\n$jsonString",
+            regex.containsMatchIn(jsonString)
+        )
+    }
+
     private val flagsFileName = "confidence_flags_cache.json"
+    private val eventsFileName = "confidence_flags_cache.json"
 }
